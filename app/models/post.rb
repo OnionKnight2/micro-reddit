fea647ef3b581c0aa810fc3d9c330a7765a410b1
rastko <vucen.rastko@gmail.com>
2023-06-18 16:22:59 +0200

    Add in commenting

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
 
   validates :title, uniqueness: true, presence: true, length: { maximum: 20 }
   validates :content, presence: true, length: { minimum: 2 }
+  validats :user_id, presence: true
 end
